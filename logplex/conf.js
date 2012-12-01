6ed56cfae6eb8fdd822f2bd14c8663c5b9287e9a
filirom1 <filirom1@gmail.com>
2012-12-01 18:47:33 +0000

    switch logplex udp port and web port

--- logplex/conf.js
+++ logplex/conf.js
@@ -3,7 +3,7 @@ var env = process.env;
 module.exports = {
   logplex: {
     hostname: env.LOGPLEX_HOST || 'localhost',
-    udpPort: env.LOGPLEX_UDP_PORT || 9996,
-    webPort: env.LOGPLEX_WEB_PORT || 9999
+    webPort: env.LOGPLEX_WEB_PORT || 9996,
+    udpPort: env.LOGPLEX_UDP_PORT || 9999
   },
 }
